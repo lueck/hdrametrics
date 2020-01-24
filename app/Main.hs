52bdc6f41d0c9cd048ad69f7ff9e60e72b51d5f0
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2020-01-24 22:46:27 +0100

    Input of CSV files with scenes, output CSV

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Options.Applicative
@@ -11,6 +12,9 @@ import Data.List
 import System.IO
 import Data.Aeson
 import qualified Data.IntMap as IntMap
+import qualified Data.Csv as Csv
+import qualified Data.Vector as V
+import Data.Char
 
 import Text.DraCor.Types
 import Text.DraCor.API
@@ -18,14 +22,20 @@ import Text.DraCor.API
 import Text.DraCor.Concomitance
 
 data Opts = Opts
-  { url :: String
-  , corpus :: String
-  , play :: String
+  { source :: DataSource
   , outpt :: Output
   , comnd :: Command
   }
 
-data Output = Raw | JSON --  | CSV
+data DataSource
+  = DraCorAPI
+  { url :: String
+  , corpus :: String
+  , play :: String
+  }
+  | ThreadUsersCSV String
+
+data Output = Raw | JSON | CSV
 
 data Command = Concomitance
   { concMaxCardinalNum :: Int
@@ -54,19 +64,12 @@ data SortOrder = NoSort | MetricOrder | CardinalityOrder
 
 opts_ :: Parser Opts
 opts_ = Opts
-  <$> strOption (long "url"
-                 <> metavar "URL"
-                 <> help "Base URL of dracor API."
-                 <> showDefault
-                 <> value "https://dracor.org/api")
-  <*> strOption (long "corpus"
-                 <> short 'c'
-                 <> help "The corpus the play is contained in."
-                 <> metavar "CORPUS")
-  <*> strOption (long "play"
-                 <> short 'p'
-                 <> help "The identifier of the play."
-                 <> metavar "PLAY")
+  <$> (dracor_
+       <|>
+       (ThreadUsersCSV <$> strOption
+         (long "thread-users-csv"
+          <> help "Get scenes from a CSV file."
+          <> metavar "FILENAME")))
   <*> ((flag Raw Raw
         (long "raw"
          <> help "Raw haskell output"))
@@ -74,10 +77,10 @@ opts_ = Opts
        (flag' JSON
         (long "json"
          <> help "JSON output"))
-       -- <|>
-       -- (flag' CSV
-       --  (long "csv"
-       --   <> help "CSV output"))
+       <|>
+       (flag' CSV
+        (long "csv"
+         <> help "CSV output. Note that CSV output only works properly for pairs of characters. This is --cardinality 2. For higher cardinality the set of characters is truncated after the second character."))
       )
   <*> subparser
   (command "concomitance"
@@ -97,6 +100,24 @@ opts_ = Opts
      (header "hdrametrics cooccurrence -- calculate cooccurence measures"
       <> progDesc "Calculate cooccurence measures for each pair of characters of a play or even for an set of characters with an arbitrary cardinality number. It is normalized by division by the count of scenes in the play."))
   )
+
+dracor_ :: Parser DataSource
+dracor_ = DraCorAPI
+  <$> strOption (long "url"
+                 <> metavar "URL"
+                 <> help "Base URL of dracor API."
+                 <> showDefault
+                 <> value "https://dracor.org/api")
+  <*> strOption (long "corpus"
+                 <> short 'c'
+                 <> help "The corpus the play is contained in."
+                 <> metavar "CORPUS")
+  <*> strOption (long "play"
+                 <> short 'p'
+                 <> help "The identifier of the play."
+                 <> metavar "PLAY")
+
+
 
 concomitanceLike_ :: String -> (Int -> Float -> Float -> Bool -> SortOrder -> Command) -> Parser Command
 concomitanceLike_ helpStr constructor = constructor
@@ -144,56 +165,72 @@ main = execParser opts >>= run
            "hdrametrics - calculate metrics of a dramatic text.")
 
 
-fetch' :: String -> String -> IO B.ByteString
+fetch :: String -> String -> IO B.ByteString
 -- fetch url path = do
 --   request <- parseRequest $ url ++ path
 --   manager <- newManager tlsManagerSettings
 --   return $ responseBody $ httpLbs request manager
-fetch' url path = simpleHttp $ url ++ path
+fetch url path = simpleHttp $ url ++ path
 
-fetch :: Opts -> (String -> IO B.ByteString)
-fetch opts = fetch' $ url opts
 
 run :: Opts -> IO ()
-run gOpts@(Opts _ _ _ _ cOpts@(Concomitance _ _ _ _ _)) =
+run gOpts@(Opts _ _ cOpts@(Concomitance _ _ _ _ _)) =
   concomitanceLikePlay concomitanceP gOpts cOpts
-run gOpts@(Opts _ _ _ _ cOpts@(Dominance _ _ _ _ _)) =
+run gOpts@(Opts _ _ cOpts@(Dominance _ _ _ _ _)) =
   concomitanceLikePlay dominanceP gOpts cOpts
-run gOpts@(Opts _ _ _ _ cOpts@(Cooccurrence _ _ _ _ _)) =
+run gOpts@(Opts _ _ cOpts@(Cooccurrence _ _ _ _ _)) =
   concomitanceLikePlay cooccurrenceP gOpts cOpts
+
+scenes :: Opts -> IO [[T.Text]]
+scenes gOpts@(Opts (DraCorAPI url corpus play) _ _) = do
+  ply' <- getPlay (fetch url) corpus play
+  case ply' of
+    Nothing -> do
+      fail "Fatal: Could not parse JSON data"
+    Just ply -> do
+      return $ map scnSpeakers $ plySegments ply
+scenes (Opts (ThreadUsersCSV fName) _ _) = do
+  c <- B.readFile fName
+  let csvOpts = Csv.defaultDecodeOptions { Csv.decDelimiter = fromIntegral (ord '\t') }
+  case (Csv.decodeWith csvOpts Csv.HasHeader c :: Either String (V.Vector (T.Text, T.Text))) of
+    Left err -> do
+      fail err
+    Right speakers -> do
+      return $ snd $ V.foldl
+        (\(lastThread, acc@(xs:xss)) (newThread, speaker) ->
+           (if newThread==lastThread
+            then (newThread, (speaker:xs):xss)
+            else (newThread, [speaker]:acc)))
+        ("", [[]])
+        speakers
 
 -- concomitanceLikePlay :: Eq a => ([a] -> [a] -> Bool) -> Opts -> Command -> IO ()
 concomitanceLikePlay predicate gOpts cOpts = do
-  ply' <- getPlay (fetch gOpts) (corpus gOpts) (play gOpts)
-  case ply' of
-    Nothing -> do
-      hPutStrLn stderr "Could find play"
-    Just ply -> do
-      let scenes = map scnSpeakers $ plySegments ply
-          characters = nub $ concat scenes
-          -- characterSets = filter ((\l -> l>=2 && l<=(concMaxCardinalNum cOpts)) . length) $
-          --   subsequences characters
-          characterSets = filter longerThanOne $
-            subsequencesOfSize (concMaxCardinalNum cOpts) characters
-          -- mapping of strings to integers
-          characterIntTuples = zip [1..] characters
-          getInt c = fst $ head $ filter ((==c) . snd) characterIntTuples
-          characterMap = IntMap.fromList $ characterIntTuples
-          sceneInts = map (map getInt) scenes
-          intSets = filter longerThanOne $
-            subsequencesOfSize (concMaxCardinalNum cOpts) [1..(length characters)]
-          -- calculate
-          concomitanceValues = foldPlayWith predicate intSets sceneInts
-          out = sortBy (concSortedBy cOpts) $ -- sortOn (sortOrder $ concSortedBy cOpts) $
-            filter ((\v -> v >= (concLowerBoundConc cOpts) &&
-                           v <= (concUpperBoundConc cOpts)) . snd) $
-            map ((mkAbsoluteValues (concAbsoluteValues cOpts) (fromIntegral $ length scenes)) . -- mk absolute values
-                 (\(is, v) -> ((map (characterMap IntMap.!) is), v)))
-            concomitanceValues
-      hPutStrLn stderr $ "Found scenes: " ++ (show $ length scenes)
-      hPutStrLn stderr $ "Found characters: " ++ (show $ length characters)
-      hPutStrLn stderr $ "Size of generated powerset of characters: " ++ (show $ length characterSets)
-      output gOpts out
+  scns <- scenes gOpts
+  let characters = nub $ concat scns
+      -- characterSets = filter ((\l -> l>=2 && l<=(concMaxCardinalNum cOpts)) . length) $
+      --   subsequences characters
+      characterSets = filter longerThanOne $
+        subsequencesOfSize (concMaxCardinalNum cOpts) characters
+      -- mapping of strings to integers
+      characterIntTuples = zip [1..] characters
+      getInt c = fst $ head $ filter ((==c) . snd) characterIntTuples
+      characterMap = IntMap.fromList $ characterIntTuples
+      sceneInts = map (map getInt) scns
+      intSets = filter longerThanOne $
+        subsequencesOfSize (concMaxCardinalNum cOpts) [1..(length characters)]
+      -- calculate
+      concomitanceValues = foldPlayWith predicate intSets sceneInts
+      out = sortBy (concSortedBy cOpts) $ -- sortOn (sortOrder $ concSortedBy cOpts) $
+        filter ((\v -> v >= (concLowerBoundConc cOpts) &&
+                       v <= (concUpperBoundConc cOpts)) . snd) $
+        map ((mkAbsoluteValues (concAbsoluteValues cOpts) (fromIntegral $ length scns)) . -- mk absolute values
+             (\(is, v) -> ((map (characterMap IntMap.!) is), v)))
+        concomitanceValues
+  hPutStrLn stderr $ "Found scenes: " ++ (show $ length scns)
+  hPutStrLn stderr $ "Found characters: " ++ (show $ length characters)
+  hPutStrLn stderr $ "Size of generated powerset of characters: " ++ (show $ length characterSets)
+  output gOpts out
   where
     sortBy MetricOrder = sortOn snd
     sortBy CardinalityOrder = sortOn (length . fst)
@@ -207,11 +244,16 @@ concomitanceLikePlay predicate gOpts cOpts = do
     mkAbsoluteValues _ _ = id
 
 -- | Format the output
-output :: (ToJSON a, Show a) => Opts -> [a] -> IO ()
-output (Opts _ _ _ Raw _) vals = do
-  mapM print vals
-  return ()
-output (Opts _ _ _ JSON _) vals = do
+output :: (ToJSON a, Csv.ToField a, Show a,
+           ToJSON i, Csv.ToField i, Show i, Fractional i) =>
+          Opts -> [([a], i)] -> IO ()
+output (Opts _ Raw _) vals = do
+  mapM_ print vals
+output (Opts _ JSON _) vals = do
   print (toJSON vals)
--- output (Opts _ _ _ CSV _) vals = do
---   print vals
+output (Opts _ CSV _) vals = do
+  -- We can only encode list of characters with fixed length, so we
+  -- take 2, only!
+  B.putStr $
+    Csv.encode $
+    map (\(cs, v) -> (head cs, head $ tail cs, v)) vals
